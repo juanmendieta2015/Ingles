e6a885c82504c2b3fb5029e7aab75f8d2928a6c0
juanmendieta2015 <juanmsn@live.com>
2018-04-08 00:46:43 -0500

    - Se ha agregado mas temas - Se retoco el diseño - Se agrego una barra de progreso

--- index.php
+++ index.php
@@ -11,11 +11,23 @@
 
     <div id="main">
         <div id="header">
-            <h1>Prácticas de Inglés</h1>
+            <h1>Prácticas de Inglés: Traducciones Inversas</h1>
         </div>
-        <div id="contador" class="contador">0</div>
+        <div id= "topic">
+            Tema:
+            <span id="topic_title"></span>
+        </div>
+        
+        <!-- <div id="contador" class="contador"></div> -->
+        <div class="progress_bar_container">
+            <div id="percent">0%</div>
+        </div>
+
         <div id="resultado" class= "resultado">Haga clic en "Generar"</div>
         
+        <div id="answer">
+        </div>
+
         <div id="footer">
             <button id="generarPregunta" class="buttoms">Generar</button>
             <button id="resetear" class="buttoms">Resetear</button>
@@ -23,13 +35,12 @@
         <div id="notification">
             <p>Se han terminado las preguntas</p>        
         </div>
-        <div id="answer">
-        </div>
         <div id="help">
             <h4>Tips:</h4>
             <ul>
                 <li>Presione "Enter" para Generar</li>
                 <li>Presione "1" para Mostrar/Ocultar la respuesta</li>
+                <li>Presione "0" para Resetear</li>
             </ul>
         </div>
     </div>
