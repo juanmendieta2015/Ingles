9e7914545ddf7a0d36755047580867fda32ffdd7
Juan Mendieta <juanmsn@live.com>
2018-04-12 15:20:32 -0500

    Incorporacion de Bootstrap
    - Actualizacion de las interfaces con Bootsrap
    - Refactorizacion de codigo

--- index.php
+++ index.php
@@ -5,51 +5,111 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ingles</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link href="css/main.css" rel="stylesheet" type="text/css">
 </head>
 <body id="body">
 
-    <div id="main">
-        <div id="header">
-            <h1>Prácticas de Inglés: Traducciones Inversas</h1>
+    <div class="container main" >
+
+        <!-- Header -->
+
+        <div class="row">
+            <div class="col-xs-12" id="header">
+                <h1>Prácticas de Inglés: Traducciones Inversas</h1>
+            </div>
         </div>
 
-        <div class= "topic">
-            Tema:
-            <span>
-                <select id="topic_id" class="topic topic_dropdown">
-                    <option value="anything">Anything</option>
-                    <option value="presente_continuo">Presente Continuo</option>
-                </select>            
-            </span>
-            <!-- <span id="topic_title"></span> -->
+
+        <!-- Tema -->
+
+        <div class="row">
+            <div class="col-xs-12 topic" >
+               Tema:
+                <span>
+                    <select id="topic_id" class="topic topic_dropdown">
+                        <option value="anything">Anything</option>
+                        <option value="presente_continuo">Presente Continuo</option>
+                    </select>            
+                </span>
+            </div>
         </div>
+
+
+
+        <!-- Progress bar -->
+
+        <div class="row">
+            <div class="col-xs-12 progress_bar_container">        
+                    <div id="percent">0%</div>
+            </div>
+        </div>
+
+
+
+
+        <!-- Pregunta -->
         
-        <div class="progress_bar_container">
-            <div id="percent">0%</div>
+        <div class="row">
+            <div class="col-xs-12 resultado" id="resultado">        
+                <h1>Haga clic en "Generar"</h1>
+            </div>
         </div>
 
-        <div id="resultado" class= "resultado">Haga clic en "Generar"</div>
+
+
+        <!-- Answer -->
         
-        <div id="answer">
+       <div class="row">
+            <div class="col-xs-12 alert alert-info alert-dismissible" role="alert" id="answer">                   
+            </div>
         </div>
 
-        <div id="footer">
-            <button id="generarPregunta" class="buttoms">Generar</button>
-            <button id="resetear" class="buttoms">Resetear</button>
+
+
+        <!-- Botones -->
+
+       <div class="row">
+            <div class="col-xs-12" id="footer" >        
+                <button id="generarPregunta" class="btn btn-success buttoms">Generar</button>
+                <button id="resetear" class="btn buttoms">Resetear</button>
+            </div>
         </div>
-        <div id="notification">
-            <p>Se han terminado las preguntas</p>        
+
+
+
+        <!-- Notificacion -->
+
+       <div class="row">
+            <div class="col-xs-12 alert alert-warning alert-dismissible" role="alert" id="notification"> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>   
+                    <p>Se han terminado las preguntas</p>        
+            </div>
         </div>
-        <div id="help">
-            <h4>Tips:</h4>
-            <ul>
-                <li>Presione "Enter" para Generar</li>
-                <li>Presione "1" para Mostrar/Ocultar la respuesta</li>
-                <li>Presione "0" para Resetear</li>
-            </ul>
-        </div>
-    </div>
+
+
+
+        <!-- Tips -->
+
+       <div class="row">
+            <div class="col-xs-12  col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"  id="help">  
+
+                <h5>Tips:</h5>
+                <ul>
+                    <li>Presione "Enter" para Generar</li>
+                    <li>Presione "1" para Mostrar/Ocultar la respuesta</li>
+                    <li>Presione "0" para Resetear</li>
+                </ul>
+
+            </div>
+        </div>                
+
+    </div>  
+
     <script src="js/main.js"></script>
+
 </body>
 </html>
