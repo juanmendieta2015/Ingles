edb79dc68e2230070b8495513a3dfce328f2df3b
Juan Mendieta <juanmsn@live.com>
2018-04-14 17:49:11 -0500

    Inclusion de un archivo JSON
    Se modifico el codigo para cargar el banco de preguntas desde un archivo externo JSON (anteriormente las preguntas estaban almacendas en arrays dentro del codigo), es decir estan de manera separada las preguntas del codigo JS

--- js/main.js
+++ js/main.js
@@ -16,47 +16,30 @@ var btnDismissHTML =    '<button type="button" class="close" data-dismiss="alert
                         '</button>';
 
 // Oculta la respuesta
-answer.style.display = "none";              
+answer.style.display = "none";   
+
+
 
 // Almacena los indices de las preguntas que van quedando disponibles
 // para su seleccion aleatoria
 disponibles=[];
 
 
-// Banco de preguntas por Temas
 
+// Carga Banco de preguntas desde Archivo JSON
 
-preguntas_anything = [
-    ["¿Quieres algo?", "Do you want anything?"],
-    ["¿Vas a decirme algo?", "Are you going to tell me anything?"],
-    ["¿Quieren algo?", "Do they want anything?"],
-    ["¿Él quiere algo de la tienda?", "Does he want anything from the shop?"],
-    ["¿Hay algo en la mesa?", "Is there anything on the table?"],
-    ["¿Harás algo esta noche?", "Are you doing anything tonight?"],
-    ["¿Hay algo en el piso?", "Is there anything on the floor?"],
-    ["¿Tienes algo en tu bolsillo?", "Do you have anything in your pocket?"],
-    ["¿Hay algo en tu vaso?", "Is there anything in your glass?"],
-    ["¿Necesitan algo del supermercado?", "Do they need anything from the supermarket?"],
-    ["¿Tienes algo en tu cartera?",   "Do you have anything in your wallet?"], 
-    ["¿Harás algo este fin de semana?", "Are you doing anything this weekend?"]
-];
+var preguntasJSON = {};
+var preguntas = {};
 
-preguntas_presente_coninuo = [
-    ["¿Qué estas haciendo?", "What are you doing?"],
-    ["¿Dónde irás esta noche?", "What are you going tonight?"],
-    ["¿Qué harás el viernes en la noche?", "What are you doing on Friday night?"],
-    ["¿Qué harás en navidad?", "What are you doing for Christmans?"],
-    ["¿Que harás en Halloween?", "What are you doing for Halloween?"],
-    ["No haré nada especial", "I'm not doing anything special"],
-    ["¿Qué harás mañana por la noche?", "What are you doing tomorrow night?"],
-    ["¿Qué harás en verano?", "What are you doing for summer?"],
-    ["¿Qué harás esta noche?", "What are you doing tonight?"],
-];
+$.getJSON('js/data.json', function(data, status) { 
+    preguntasJSON = data;
 
-// Banco de preguntas por default
-preguntas = preguntas_anything;            
+    // Seteo del tema por default
+    preguntas = preguntasJSON.preguntas_anything; 
 
-resetearPreguntas();
+    resetearPreguntas();
+})
+
 
 
 // Boton Resetear 
@@ -66,11 +49,13 @@ resetear.addEventListener('click', function(){
 })
 
 
+
 // Boton Generar
 
 btnGenerarPregunta.addEventListener('click', function(){
     generarPreguntas();
 })
+
 
 
 // Captura eventos keydown
@@ -98,6 +83,7 @@ body.addEventListener('keydown', function(){
 })
 
 
+
 // Dropdown de temas
 
 cbmTopicId.addEventListener('change', function(){
@@ -105,13 +91,13 @@ cbmTopicId.addEventListener('change', function(){
     switch (cbmTopicId.value) {
 
         case 'anything':
-            preguntas = preguntas_anything;
-            // topic_title.innerHTML = "Anything";
+            // preguntas = preguntas_anything;
+            preguntas = preguntasJSON.preguntas_anything;
             break;
 
         case 'presente_continuo':
-            preguntas = preguntas_presente_coninuo;
-            // topic_title.innerHTML = "Presente Contínuo";
+            preguntas = preguntasJSON.presente_continuo;
+            break;
     }    
 
     disponibles=[];
@@ -121,7 +107,7 @@ cbmTopicId.addEventListener('change', function(){
 
 
 
-// Selecciona aleatoriamente un elemento del array disponibles[]
+// Seleccion aleatoria de un elemento en disponibles[]
 
 function generarAleatorio(){
     var item = Math.floor(Math.random() * disponibles.length);
@@ -130,7 +116,7 @@ function generarAleatorio(){
 
 
 
-// Elimina del array disponible[] la pregunta generada actualmente
+// Elimina de disponible[] la pregunta generada actualmente
 // para que no se vuelva ha repetir de la seleccion
 
 function eliminarElemento(item){
@@ -145,9 +131,11 @@ function eliminarElemento(item){
 
 function resetearPreguntasDisponibles(){
     disponibles = [];
+
     preguntas.forEach(function(element, index){
         disponibles[index] = index;
     });
+
 }
 
 
@@ -160,21 +148,17 @@ function generarPreguntas(){
         let itemPreguntas = disponibles[item];
         
         // Muestra la pregunta
-        resultado.innerHTML = preguntas[itemPreguntas][0];
+        resultado.innerHTML = preguntas[itemPreguntas]["question"];
         resultado.style.color = "black";  
 
-        
         // Prepara la respuesta para que el usuario la muestre/oculte
-        answer.innerHTML = btnDismissHTML + "<h2>" + preguntas[itemPreguntas][1] + "</h2>";
-        
+        answer.innerHTML = btnDismissHTML + "<h2>" + preguntas[itemPreguntas]["answer"] + "</h2>";
 
         // Mantiene/Anula la configuracion de usuario mostrar/ocultar respuesta
         answer.style.display = "none";
 
-
         // contador
         count += 1;
-        
 
         // Actualiza el porcentaje
         new_percent = Math.round( (count / preguntas.length) * 100 );
